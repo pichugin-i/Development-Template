ecfca258abb849722e23d056b6619ef7aa12c003
pichugin-i <pichugin.lua@yandex.ru>
2020-02-26 18:24:09 +0300

    add realiz time

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,35 @@
-#include "add.h"
+// Copyright 2020 pichugin.i
 #include <iostream>
+#include <clocale>
+#include <cstdlib>  // Для реалистичности времени в консоли.
+#include <string>
+#include "ClassTime.h"
+#include <windows.h>
+
+using namespace std;
 
 int main() {
-  std::cout << "5 + 5 = " << add(5, 5) << std::endl;
+  setlocale(LC_ALL, "Russian");
+  int w;
+  Time time(23, 15, 59); // для конструктора инициализации.
+  time.plustime(); // +1 секунда в секунду xD
+  time.minustime(); // -1 секунда в секунду.
+  cout << "Исходное время:" << endl;
+  time.Show();
+  cout << endl;
+  cout << "Свойства:" << endl << "Введите * 1 * , чтобы изменить время." << endl << "Введите * 2 * , чтобы выйти." << endl;
+  cout << "Ввод: ";
+  cin >> w;
+  w = wibor(w);
+  if (w == 1) {
+    cin >> time;
+    time.Show();
+    time.plustime(); // +1 секунда в секунду xD
+    time.minustime(); // -1 секунда в секунду.
+    cout << "Исходное время:" << endl;
+    time.Show();
+  }
+  cout << endl;
+  system("pause");
+  return 0;
 }
--- include/add.h
+++
@@ -1,6 +0,0 @@
-#ifndef INCLUDE_ADD_H_
-#define INCLUDE_ADD_H_
-
-int add(int x, int y);
-
-#endif  // INCLUDE_ADD_H_
