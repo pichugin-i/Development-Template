6ff793ba6bffeb9c368d51f9aca466cba27ed295
pichugin-i <pichugin.lua@yandex.ru>
2020-04-07 15:45:11 +0300

    add

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,23 @@
 #include "add.h"
-#include <iostream>
 
+using namespace std;
+
+NOTIFYICONDATA pnid;
 int main() {
-  std::cout << "5 + 5 = " << add(5, 5) << std::endl;
+  SetConsoleTitle("TextEditor");
+  setlocale(LC_ALL, "Russian");
+  char str[300];
+  TextEditor a;
+  TextEditor t1;
+  TextEditor put;
+  TextEditor show;
+  a.options();
+  put.TXT();
+  t1.position(1, 4);
+  cout << "¬ведите текст:";
+  t1.position(1, 5);
+  cout << ">> ";
+  cin.getline(str, 76, '.');
+  system("CLS");
+  show.showw(str);
 }
--- include/add.h
+++ include/add.h
@@ -1,6 +1,30 @@
 #ifndef INCLUDE_ADD_H_
 #define INCLUDE_ADD_H_
+#pragma once
+#include <iostream>
+#include <clocale>
+#include "Header.h"
+#include <conio.h>
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
+#include <Windows.h>
 
-int add(int x, int y);
+using namespace std;
 
-#endif  // INCLUDE_ADD_H_
+class TextEditor {
+private:
+  // x,y-позиция ввода текста, len - длина ввода текста в поле.
+  int x;
+  int y;
+public:
+  void options();
+  void position(int xpos, int ypos);
+  void TXT();
+  void showw(char *str);
+  TextEditor();
+  TextEditor(const TextEditor &c);
+  TextEditor(int _x, int _y);
+  friend ostream& operator << (ostream& stream, const TextEditor& a);
+  friend istream& operator >> (istream& stream, TextEditor& a);
+};
